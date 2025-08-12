e9503cc83ce5d450940116c6bc90d82b808ab267
LucasLem0s <lucaslemoss@live.com>
2025-08-12 20:46:44 -0300

    Revisão JS

---
+++ main.js
@@ -0,0 +1,21 @@
+/*function soma (a,b){
+    return a+b
+}
+console.log(soma(5,2))
+*/
+//Lista
+var lista = [1,2,3,4]
+console.log(lista)
+console.log(lista[0])
+
+//Jason Array
+var lista2=[
+    {
+        "nome":"Teste","idade":5
+    },
+    {
+        "nome":"Teste","idade":6
+    }
+
+
+]
